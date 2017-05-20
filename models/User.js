962b124d3b59d7077b62127d2b66768a53cb3230
Steven Palomino <steven.palomino@speakcreative.com>
2017-05-20 14:27:12 -0500

    models

---
+++ models/User.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  hashtags: [],
+  rate: Number,
+  likes: Number,
+  jobExpiryKey: String
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = User;
